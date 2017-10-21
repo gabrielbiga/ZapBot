9f14defff1bf538d2baf3d154f910b4b1e8f8ffb
Gabriel Matos <gabrielbiga@me.com>
2017-10-21 12:09:11 -0200

    Critical bug fixes and enhancements
    Bugs:
    - Resolvido o bug no recebimento de mensagens onde o bot travava depois de ter recebido entre 4 e 8 mensagens.
    - Resolvido bug que o BOT dava crash ao rolar a página
    - Resolvido bug que o BOT processava mensagens aleatórias ao rolar a página
    
    Melhorias:
    - Agora mensagens enviadas pelo proprio bot nao trigam o evento onReceive
    - Mais documentação no código

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,7 @@
 (() => {
+    /**
+     * Small event emitter implementation
+     */
     class EventEmitter<T>
     {
         private events: Map<string, Array<EventListenerFn<T>>> = new Map();
@@ -18,20 +21,122 @@
         }
     }
 
+    /**
+     * Buffer implementation with a fixed value size
+     */
+    class PeekBuffer<T extends Bufferable>
+    {
+        private _buffer: Array<T> = [];
+        private _newerItem: Date;
+
+        constructor(private _maxSize: number = 50)
+        { }
+
+        /**
+         * Append just unique items
+         *
+         * @param item The item
+         */
+        public append(item: T): boolean
+        {
+            if (this.exists(item.digest)) return false;
+
+            if (this._buffer.length >= this._maxSize) this._buffer.shift();
+
+            this._buffer.push(item);
+            this._allignBufferHistory(item);
+            return true;
+        }
+
+        public size(): number
+        {
+            return this._buffer.length;
+        }
+
+        public exists(digest: number): boolean
+        {
+            return this._buffer.find(item => item.digest === digest) !== undefined;
+        }
+
+        /**
+         * Store the newer item
+         *
+         * @param newItem Latest item
+         */
+        private _allignBufferHistory(newItem: T): void
+        {
+            if (!this._newerItem)
+            {
+                this._newerItem = newItem.date;
+                return;
+            }
+
+            if (newItem.date > this._newerItem) this._newerItem = newItem.date;
+        }
+
+        /**
+         * Get a full array, take its difference and merge with the local buffer.
+         *
+         * @param origin The full new array
+         */
+        public diffAndMerge(origin: Array<T>): Array<T>
+        {
+            const startNewerItem: Date = this._newerItem || new Date();
+
+            // Minute tolerance
+            startNewerItem.setMinutes(startNewerItem.getMinutes() - 1);
+
+            return origin
+                // Add the new items to the buffer
+                .filter((item: T) => this.append(item))
+                // Just newer messages
+                .filter((item: T) => item.date >= startNewerItem);
+        }
+    }
+
+    /**
+     * ZapBot Implementation
+     */
     return window['ZapBot'] = class ZapBot
     {
+        // Already started?
+        private _firstRun: boolean = true;
+
+        // All messages processed
+        private _messageBuffer: PeekBuffer<ReceiveEvent> = new PeekBuffer();
+
+        // All messages emitted by the bot
+        private _messagesEmitted: SimpleBufferFastSeek<string> = {};
+
+        // All tasks processed by the bot
+        private _tasksProcessed: SimpleBufferFastSeek<boolean> = {};
+
+        // Event emitter for all messages
         public readonly event: EventEmitter<BotEvent> = new EventEmitter();
-        private _lastReceivedMessageCount: number = 0;
+
+        // Minute tolerance to process tasks. (value in minutes)
+        // You can tweak this value if you want to handle SPAM atacks
+        public minuteTolerance: number = 1;
 
         constructor(
+            // Process messages before bot has been started
+            // @experimental
             private _processRetroactive: boolean = false,
+
+            // Start the class listening
             private _startListening: boolean = false,
+
+            // Listen interval
+            // Be careful: Lower times can crash your browser
             private _listenInterval: number = 2000
         )
         {
             if (this._startListening) this.startListen();
         }
 
+        /**
+         * Start listen to new messages
+         */
         public startListen(): void
         {
             if (!this._startListening) setInterval(
@@ -41,7 +146,11 @@
         }
 
         /**
+         * Send a simple message to contact or group
+         *
          * From: https://gist.github.com/mathloureiro/4c74d60f051ed59650cc76d1da0d32da by @mathloureiro
+         *
+         * @param messsage Your message
          */
         public sendMessage(message: string): void
         {
@@ -50,6 +159,10 @@
                 'div.pluggable-input.pluggable-input-compose',
                 'div.pluggable-input-body.copyable-text.selectable-text'
             ].join('>'));
+
+            // Shit happens...
+            if (!textBox) throw new ReferenceError('Aconteceu algum problema ao enviar essa mensagem');
+
             const event: Event = new (window['Event'] || window['InputEvent'])('input', {
                 bubbles: true
             });
@@ -61,39 +174,84 @@
             (document.querySelector('button.compose-btn-send') as HTMLElement).click();
 
             this.event.emit('onSend', { message, date });
+            this._messagesEmitted[this.hashCode(message, true)] = message;
         }
 
+        /**
+         * Process new messages and trigger the event
+         */
         private listenReceiveMessage(): void
         {
-            const lastMessages: NodeListOf<Element> = document.querySelectorAll('.message-in, .message-out');
-            const lastMessagesLen: number = lastMessages.length;
+            // Search new messages on screen
+            const lastMessages: Array<Element> = Array.from(document.querySelectorAll('.message-in, .message-out'));
 
-            if (this._lastReceivedMessageCount === 0 && !this._processRetroactive)
+            // Parse the new data
+            const receivedMessages: Array<ReceiveEvent> = lastMessages.map((bubbleElem: Element) =>
             {
-                console.warn(`Starting now... Omitting about ${lastMessagesLen} old messages`);
-                this._lastReceivedMessageCount = lastMessagesLen;
-                return;
-            }
-
-            if (this._lastReceivedMessageCount >= lastMessagesLen) return;
-
-            console.warn(`New ${lastMessagesLen - this._lastReceivedMessageCount} messages!`);
-
-            for (let i = this._lastReceivedMessageCount; i < lastMessagesLen; i++)
-            {
-                const bubbleElem: Element = lastMessages[i];
                 const bubbleMetadata: string = bubbleElem.querySelector('.bubble').getAttribute('data-pre-plain-text');
 
-                this.event.emit('onReceive', {
-                    rawElement: bubbleElem,
+                const parsedMessage: ReceiveEvent = {
                     me: bubbleElem.classList.contains('message-out'),
                     message: (bubbleElem.querySelector('.selectable-text') as HTMLElement).innerText,
                     sender: bubbleMetadata.split(']').pop().replace(':', '').trim(),
-                    date: new Date(bubbleMetadata.split(']').shift().replace('[', '').trim())
-                });
+                    date: new Date(bubbleMetadata.split(']').shift().replace('[', '').trim()),
+                    digest: 0,
+                    processDigest: this.hashCode(bubbleElem.querySelector('.message-text').getAttribute('data-id'))
+                };
+
+                parsedMessage.digest += this.hashCode(parsedMessage);
+                parsedMessage.rawElement = bubbleElem;
+
+                return parsedMessage;
+            })
+            // Filter messages dont sended by the bot
+            .filter((newMessage: ReceiveEvent) => this._messagesEmitted[this.hashCode(newMessage.message, true)] === undefined);
+
+            // Is the first run?
+            // FIXME: This flag(_processRetroactive) is so fucking bugged. I have no idea why I did this shit.
+            if (this._firstRun && !this._processRetroactive)
+            {
+                console.warn(`Starting now... Omitting about ${lastMessages.length} old messages`);
+
+                // Flaging as processed
+                receivedMessages.forEach(message => this._tasksProcessed[message.processDigest] = true);
+
+                this._firstRun = false;
+                return;
             }
 
-            this._lastReceivedMessageCount = lastMessagesLen;
+            // Just fresh messages
+            const newMessages: Array<ReceiveEvent> = this._messageBuffer.diffAndMerge(receivedMessages)
+                .filter(message => this._tasksProcessed[message.processDigest] === undefined);
+
+            // Emit changes to the bot implementation
+            newMessages.forEach(message =>
+            {
+                this.event.emit('onReceive', message);
+                this._tasksProcessed[message.processDigest] = true;
+            });
+        }
+
+        /**
+         * Calculates a checksum of the given object
+         *
+         * @param obj Your object
+         * @param [literal] Remove all special chars
+         */
+        private hashCode<T>(obj: T, literal?: boolean): number
+        {
+            let str: string = JSON.stringify(obj);
+
+            if (literal) str = str.replace(/[^a-zA-Z0-9]/g, '');
+
+            return str
+                .split('')
+                .reduce((a: number, b: string) =>
+                {
+                    a = ( ( a << 5 ) - a ) + b.charCodeAt(0);
+
+                    return a & a;
+                }, 0);
         }
     }
 })();
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,20 +1,73 @@
-declare type EventListenerFn<T> = (param: T) => void;
+/**
+ * Event function prototype for EventEmitter
+ */
+declare type EventListenerFn<T> = (meta: T) => void;
 
+/**
+ * Simple key value buffer that is faster to seek
+ */
+declare interface SimpleBufferFastSeek<T>
+{
+    [digest: number]: T;
+}
+
+/**
+ * Bot supported events
+ */
 declare type BotEvent =
     | SendEvent
     | ReceiveEvent;
 
-declare interface SendEvent
+/**
+ * Bufferable types in PeekBuffer
+ */
+declare interface Bufferable
 {
-    message: string;
+    // Date of the item
     date: Date;
+
+    // Checksum of the object
+    digest: number;
 }
 
-declare interface ReceiveEvent
+/**
+ * Metadata triggered by onReceive
+ */
+declare interface ReceiveEvent extends Bufferable
 {
+    /**
+     * Who is the message sender?
+     * Note: If you dont have him in your contact list, it will be the number (with county code)
+     */
     sender: string;
+
+    // The message
     message: string;
-    rawElement: Element;
+
+    // DOM Element
+    rawElement?: Element;
+
+    // Date of the message
     date: Date;
+
+    // Is from myself?
     me: boolean;
+
+    // Unique checksum for this message
+    digest: number;
+
+    // Process digest provided by the system
+    processDigest: number;
+}
+
+/**
+ * Metadata triggered by onSend
+ */
+declare interface SendEvent
+{
+    // The message
+    message: string;
+
+    // Date of the message
+    date: Date;
 }
